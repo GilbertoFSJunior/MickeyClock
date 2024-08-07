bfa7f00c9630069da0666727aead896e9a2a88e6
GilbertoFSJunior <142622373+GilbertoFSJunior@users.noreply.github.com>
2024-08-07 11:51:00 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
+
+//Selecionando os elementos  DOM
 var hours = document.getElementById("hours");
 var minutes = document.getElementById("minutes");
 var seconds = document.getElementById("seconds");
 
+//Função para atualizar o Relógio
 function updateClock() {
   var now = new Date();
   var currentHour = now.getHours();
   var currentMinutes = now.getMinutes();
   var currentSeconds = now.getSeconds();
 
+  //Definindo a posição dos Ponteiros das horas e dos minutos
   if (currentHour >= 6 && currentHour < 12) {
     hours.className = "left";
   } else {
@@ -20,11 +24,14 @@ function updateClock() {
     minutes.className = "left";
   }
 
+  //Definindo a rotação dos ponteiros
   set(hours, 30 * currentHour + 0.5 * currentMinutes); // Ajuste para considerar os minutos
   set(minutes, 6 * currentMinutes);
   set(seconds, 6 * currentSeconds);
 }
 
+
+//Função para aplicar a rotação nos elementos
 function set(element, degree) {
   element.style.transform = "rotate(" + degree + "deg)";
   element.style.webkitTransform = "rotate(" + degree + "deg)";
